88bca8b47650b424fbeebe5f44fc8d990b3c0a84
kalaiyarazan <kalaiyarazan.v@gmail.com>
2020-02-22 21:09:09 +0530

    change navbar as func component

--- src/component/layout/Navbar.js
+++ src/component/layout/Navbar.js
@@ -1,27 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Navbar extends Component {
-  //Defaul Props in class Component
-  static defaultProps = {
-    icon: 'fab fa-github fa-2x',
-    title: 'Github Finder'
-  };
-
-  render() {
-    return (
-      <nav className='navbar'>
-        <i className={this.props.icon} />
-        <h1>{this.props.title}</h1>
-      </nav>
-    );
-  }
-}
-
-//PropType Check
-Navbar.propTypes = {
-  icon: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired
+const Navbar = ({ icon = 'fab fa-github fa-2x', title = 'Github Finder' }) => {
+  return (
+    <nav className='navbar'>
+      <i className={icon} />
+      <h1>{title}</h1>
+    </nav>
+  );
 };
 
 export default Navbar;
