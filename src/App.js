85dafa2d08e6a6787c4824e0f2eff692afe40046
kalaiyarazan <kalaiyarazan.v@gmail.com>
2020-02-22 21:42:27 +0530

    created state in App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './component/layout/Navbar';
+import Users from './component/users/Users';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
+    const { users, loading } = this.state;
     return (
       <div className='App'>
         <Navbar />
-        <h1>App Component</h1>
+        <Users users={users} loading={loading} />
       </div>
     );
   }
