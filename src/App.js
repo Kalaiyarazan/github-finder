535185f2561cf14dfb97c756f149c15f7d4e9241
kalaiyarazan <kalaiyarazan.v@gmail.com>
2020-02-23 12:06:10 +0530

    Removed Did Mount and added Alert State

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import Navbar from './component/layout/Navbar';
 import Users from './component/users/Users';
 import axios from 'axios';
 import Search from './component/users/Search';
+import Alert from './component/layout/Alert';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: {}
   };
-
-  async componentDidMount() {
-    this.setState({ loading: true });
-    const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    this.setState({ users: res.data, loading: false });
-  }
 
   searchUsers = async text => {
     this.setState({ loading: true });
@@ -27,12 +21,25 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  setAlert = (message, type) => {
+    console.log(message, type);
+    this.setState({ alert: { message, type } });
+  };
+
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, alert } = this.state;
     return (
       <div className='App'>
         <Navbar />
-        <Search searchUsers={this.searchUsers} />
+        <Alert alert={alert} />
+        <Search
+          searchUsers={this.searchUsers}
+          clearUsers={this.clearUsers}
+          showClear={users.length > 0 ? true : false}
+          setAlert={this.setAlert}
+        />
         <Users users={users} loading={loading} />
       </div>
     );
