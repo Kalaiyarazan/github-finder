3f84e49a57ba63a9ee57fe0b3fb2f878f8670945
kalaiyarazan <kalaiyarazan.v@gmail.com>
2020-02-25 12:56:23 +0530

    Changed Export Default

--- src/component/repos/Repos.js
+++ src/component/repos/Repos.js
@@ -2,9 +2,11 @@ import React from 'react';
 import RepoItem from './RepoItem';
 import PropTypes from 'prop-types';
 
-export const Repos = ({ repos }) =>
+const Repos = ({ repos }) =>
   repos.map(repo => <RepoItem repo={repo} key={repo.id} />);
 
 Repos.propTypes = {
   repos: PropTypes.array.isRequired
 };
+
+export default Repos;
